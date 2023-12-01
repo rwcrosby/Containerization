546fa2491d641245ac8f89cebe2a67ea7d99242a
Ralph Crosby <rcrosby@camgian.com>
2023-12-01 16:26:11 -0500

    Dynamically get wsl ip address

--- 2_Kubernetes/0_k3s_coreos/ips2env.sh
+++ 2_Kubernetes/0_k3s_coreos/ips2env.sh
@@ -5,7 +5,7 @@ get_ip() {
     arp.exe -a | grep $(echo $1 | sed s/:/-/g) | awk '{print $1;}'
 }
 
-export wsl_ip=$(get_ip "00:15:5d:ad:4b:4f")
+export wsl_ip=$(ip route get 8.8.8.8 | grep eth0 | awk '{print $7}')
 export vm1_ip=$(get_ip "00-15-5d-38-01-0b")
 export vm2_ip=$(get_ip "00-15-5d-38-01-0c")
 
